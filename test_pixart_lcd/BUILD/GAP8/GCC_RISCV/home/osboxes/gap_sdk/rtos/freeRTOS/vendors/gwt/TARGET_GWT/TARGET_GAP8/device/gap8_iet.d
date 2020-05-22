59ea16dc51f74e7a95d2f5101d901c3b013605e5
schautsang <schautsang@gmail.com>
2020-05-22 08:56:08 +0800

    fix bug

--- test_pixart_lcd/BUILD/GAP8/GCC_RISCV/home/osboxes/gap_sdk/rtos/freeRTOS/vendors/gwt/TARGET_GWT/TARGET_GAP8/device/gap8_iet.d
+++
@@ -1,3 +0,0 @@
-/home/osboxes/gap_sdk/examples/pmsis/test_periph/test_pixart_lcd/BUILD/GAP8/GCC_RISCV//home/osboxes/gap_sdk/rtos/freeRTOS/vendors/gwt/TARGET_GWT/TARGET_GAP8/device/gap8_iet.o: \
- /home/osboxes/gap_sdk/rtos/freeRTOS/vendors/gwt/TARGET_GWT/TARGET_GAP8/device/gap8_iet.S \
- /home/osboxes/gap_sdk/rtos/freeRTOS/freertos_kernel/portable/GCC/RI5CY-GAP8/chip_specific_extensions/gap8/freertos_risc_v_chip_specific_extensions.h
